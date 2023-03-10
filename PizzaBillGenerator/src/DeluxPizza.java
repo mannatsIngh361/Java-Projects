dab8c45e200a79d55f5c524c334c0231432a6c37
sukhman12singh <singhmannatpreet0@gmail.com>
2023-03-10 15:37:44 +0530

    DeluxPizza Class

---
+++ PizzaBillGenerator/src/DeluxPizza.java
@@ -0,0 +1,16 @@
+public class DeluxPizza extends Pizza {
+
+    public DeluxPizza(Boolean veg) {
+        super(veg);
+        super.addExtraCheese();
+        super.addExtraTopping();
+
+    }
+    public void addExtraCheese(){
+
+    }
+    
+    public void addExtraTopping(){
+
+    }
+}
